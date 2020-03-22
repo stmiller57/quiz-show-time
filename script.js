aa6e0096ad937075521092a70cd07fab225a6854
Reformed Scribe <stmiller57@gmail.com>
2020-03-22 14:58:32 -0400

    Fixed timer so it goes to zero

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var timer = 60;
 var countdown = setInterval(function () {
     document.getElementById("counter").innerHTML = timer;
     timer--;
-    if (timer === 0) {
+    if (timer < 0) {
         clearInterval(countdown);
     }
 }, 1000);
