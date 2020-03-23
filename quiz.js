7e610d926162ea5dad566b01561b91ec1e93b3fe
Reformed Scribe <stmiller57@gmail.com>
2020-03-23 09:18:31 -0400

    Moved my quiz out of HTML and into JS after consultation with an online tutor

---
+++ quiz.js
@@ -0,0 +1,17 @@
+var myQuestions = [
+    {
+        question: "Who has won the most Super Bowl MVPs",
+        choices: ["Tom Brady", "Joe Montana", "Terry Bradshaw", "Troy Aikman"],
+        answer: "Tom Brady"
+    },
+    {
+        question: "What is the single-season record for home runs by one player?",
+        choices: ["66", "61", "70", "73"],
+        answer: "73"
+    },
+    {
+        question: "How many NBA MVP awards did Michael Jordan win?",
+        choices: ["3", "8", "5", "6"],
+        answer: ["5"],
+    },
+]
